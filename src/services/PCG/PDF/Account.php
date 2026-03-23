3a0f44a159cbd9edaf1019dec1f142bccb807163
SVGAnimate <glash.gnome@gmail.com>
2026-03-23 23:53:55 +0100

    Add Account class for accounting services

---
+++ src/services/PCG/PDF/Account.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @domain Developpement
+ * @category Accounting
+ */
+namespace Ingestion\Services\PCG\PDF;
+
+/**
+ * Reglement de type Revision, consolidation, release ?
+ */
+class Account {
+	public $code = "";
+	public $label = "";
+	//public $systeme = "";
+	public \ArrayObject $accounts;
+
+	public function __construct(string $code, string $label, ?\ArrayObject $accounts = Null) {
+		$this->code = $code;
+		$this->label = $label;
+		$this->accounts = $accounts ? $accounts : new \ArrayObject();
+	}
+	public function getLevel() {
+		return strlen($this->code);
+	}
+}
