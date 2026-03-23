eb1546a28cae65921a253d43615127dd2afc4bc0
SVGAnimate <glash.gnome@gmail.com>
2026-03-23 23:40:34 +0100

    Implement Scraper class for version management
    Added a Scraper class with methods for downloading, extracting, checking, and pushing versions.

--- src/services/PCG/Scraper.php
+++ src/services/PCG/Scraper.php
@@ -1,2 +1,19 @@
 <?php
+namespace Ingestion\Service\PCG;
 
+/**
+ * A Scraper - automate extraction
+ */
+class Scraper
+{
+  static $config;
+  
+  static public function Download($version='latest') {
+  }
+  static public function Extract($version='latest') {
+  }
+  static public function Check($version='latest') {
+  }
+  static public function Push($version='latest') {
+  }
+}
