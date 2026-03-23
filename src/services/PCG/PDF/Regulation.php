f370496e0c2056d5bbc5fe9c2f305c5bc957f63b
SVGAnimate <glash.gnome@gmail.com>
2026-03-23 23:51:21 +0100

    Create Regulation.php

---
+++ src/services/PCG/PDF/Regulation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @domain Developpement
+ * @category Accounting
+ */
+namespace Ingestion\Service\PCG\PDF;
+
+/**
+ * Reglement de type Revision, consolidation, release ?
+ */
+class Regulation
+{
+}
