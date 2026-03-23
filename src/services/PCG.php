c351bb22fd6b3a66d33b4b833946a7b15bc68a13
SVGAnimate <glash.gnome@gmail.com>
2026-03-23 23:45:07 +0100

    Update PCG.php

--- src/services/PCG.php
+++ src/services/PCG.php
@@ -2,35 +2,22 @@
 /**
  * @domain Developpement
  * @category Accounting
- *
- * 1 - Service de collect de donnée (cron ?)
- *     Download PCG--1erJanvier-{YEAR}.pdf
- *     Converte PDF to SVG/HTML
- * 2 - Service d'extraction des données collecté
- *     Scan
- *     Parse
- *     Format
- * 3 - Service de publication des donnée
- *     Export to JSON, XML, SQL
- * 4 - exploitation des donnée
- *     diff, merge, ...
- *     IA, cohérence, ...
  */
-namespace PCG\Ingestion;
+namespace Ingestion\Service;
 
 /**
- * Agent for Service PCG Scrap - A Scraper automate extraction
+ * Service PCG
  */
 class PCG
 {
   static $config;
   
-  static public function Download($version='last') {
+  static public function ScrapPdfANC($version='last') {
   }
-  static public function Extract($version='last') {
+  static public function ScrapPdfGouv($version='last') {
   }
-  static public function Check($version='last') {
+  static public function ScrapDataGouv($version='last') {
   }
-  static public function Push($version='last') {
+  static public function LookPush($version='last') {
   }
 }
